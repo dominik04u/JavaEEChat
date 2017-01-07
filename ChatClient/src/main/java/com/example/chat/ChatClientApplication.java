5abe8900b7b95c13139607ea1ce61c7b5c38604a
Dominik Adamski <dominik04u@o2.pl>
2017-01-07 20:55:38 +0100

    poprawa properties

--- ChatClient/src/main/java/com/example/chat/ChatClientApplication.java
+++ ChatClient/src/main/java/com/example/chat/ChatClientApplication.java
@@ -7,14 +7,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.example.chat.panels.Application;
 
-
 @Configuration
 @SpringBootApplication
 @EnableScheduling
 public class ChatClientApplication {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(Application.class)
+		new SpringApplicationBuilder(ChatClientApplication.class)
         .headless(false)
         .web(false)
         .run(args);
